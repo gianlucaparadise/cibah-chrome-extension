7f03e9bde47f12279a5aab3391f8c08e1fdcad98
gianlucaparadise <gianlucaparadise@gmail.com>
2020-09-24 07:14:18 +0200

    Geocoder completed using OpenCageData

--- immobiliare/inject.js
+++ immobiliare/inject.js
@@ -2,9 +2,17 @@ log(() => "🏠 Cibah 🚉 running");
 
 const listOfAds = $(".annunci-list .listing-item.js-row-detail");
 
-$(listOfAds).each((index, element) => {
-	const title = $(element).find(".titolo").text();
+$(listOfAds).each(async (index, element) => {
+	const title = $(element).find(".titolo").text().trim();
 	const listingFeatures = $(element).find(".listing-features");
 
 	$(listingFeatures).append('<li class="lif__item">🚉</li>');
+
+	const location = await getLocationFromAddress(title);
+	if (location == null) {
+		log(() => `Can't find location for: ${title}`);
+		return;
+	}
+
+	log(() => `Title: ${title}, Location: ${location.latitude},${location.longitude}`);
 });
