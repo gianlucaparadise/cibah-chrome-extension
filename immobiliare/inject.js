a0112eebcf43b2947887399803078004d5476427
gianlucaparadise <gianlucaparadise@gmail.com>
2020-10-16 15:44:36 +0200

    Better presentation

--- immobiliare/inject.js
+++ immobiliare/inject.js
@@ -1,35 +1,30 @@
 log(() => "🏠 Cibah 🚉 running");
 
-// log(() => `Setting up the tooltip`);
+const cibahSubwaysTooltipClass = "cibah-item";
+const cibahZeroSubwaysClass = "cibah-item-no-subways";
+
 // Setting up the tooltip
 $(document).tooltip({
-	items: ".cibah-item",
+	items: `.${cibahSubwaysTooltipClass}`,
 	content: function () {
 		let element = $(this);
-		if (!element.is(".cibah-item")) return;
+		if (!element.is(`.${cibahSubwaysTooltipClass}`)) return;
 
 		/**
 		 * @type {SubwayDistance[]}
 		 */
 		let distanceToSubways = element.data("cibah");
 
-		log(() => `Tooltip: Distance to Subways`);
-		log(() => distanceToSubways);
-
 		if (distanceToSubways == null || distanceToSubways.length <= 0) {
 			return "No Subways within 1km";
 		}
 
-		const closestSubway = distanceToSubways[0];
-		const name = closestSubway.subway.name;
-		const minutes = closestSubway.distanceMinutes.toFixed(0);
-		const meters = closestSubway.distanceMeters.toFixed(0);
-		return `${name} ${minutes} min (${meters}m)`;
-
-		// return "<img class='map' alt='" + text +
-		// 	"' src='http://maps.google.com/maps/api/staticmap?" +
-		// 	"zoom=11&size=350x350&maptype=terrain&sensor=false&center=" +
-		// 	text + "'>";
+		return distanceToSubways.map(s => {
+			const name = s.subway.name;
+			const minutes = s.distanceMinutes.toFixed(0);
+			const meters = s.distanceMeters.toFixed(0);
+			return `${name} ${minutes} min (${meters}m)`;
+		}).join("<br/>");
 	}
 });
 
@@ -39,14 +34,16 @@ $(document).tooltip({
  * @param {SubwayDistance[]} distanceToSubways List of subways and their distances
  */
 function addSubwayItem(root, distanceToSubways) {
-	// if (distanceToSubways != null && distanceToSubways.length > 0) {
 	// Creating the HTML item
-	const newItem = $('<li class="lif__item cibah-item">🚉</li>');
+	const hasSubways = distanceToSubways && distanceToSubways.length > 0;
+	const closest = hasSubways ? `${distanceToSubways[0].distanceMinutes.toFixed(0)} min` : "No subways";
+	const itemClass = hasSubways ? cibahSubwaysTooltipClass : cibahZeroSubwaysClass;
+
+	const newItem = $(`<li class="lif__item ${itemClass}">🚉 ${closest}</li>`);
 	newItem.data("cibah", distanceToSubways);
 
 	const listingFeatures = $(root).find(".listing-features");
 	$(listingFeatures).append(newItem);
-	// }
 }
 
 async function run() {
@@ -57,7 +54,11 @@ async function run() {
 
 		const title = $(element).find(".titolo").text().trim();
 
-		chrome.runtime.sendMessage({ address: title }, (response) => addSubwayItem(element, response));
+		chrome.runtime.sendMessage({ address: title }, (response) => {
+			log(() => `Received subways for: ${title}`);
+			log(() => response);
+			addSubwayItem(element, response)
+		});
 	}
 }
 
